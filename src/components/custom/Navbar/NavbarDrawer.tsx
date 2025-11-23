5242116dfb658113a88c03373b3ff04c08212d21
Amdadul Haque <amdadulhaque0041@gmail.com>
2025-11-23 19:20:30 +0600

    shadcn scroll bar vanish on windo open solve

--- src/components/custom/Navbar/NavbarDrawer.tsx
+++ src/components/custom/Navbar/NavbarDrawer.tsx
@@ -1,25 +1,32 @@
-import ModeToggle from "@/components/theme/mode-toggler"
-import { Button } from "@/components/ui/button"
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
+"use client";
+import ModeToggle from "@/components/theme/mode-toggler";
+import { Button } from "@/components/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 
 import {
   Sheet,
   SheetClose,
   SheetContent,
-  
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet"
-import { NavItemType } from "@/Types/ComponentTypes"
-import { ChevronDown, Menu, ShoppingCart } from "lucide-react"
-import Link from "next/link"
+} from "@/components/ui/sheet";
+import { NavItemType } from "@/Types/ComponentTypes";
+import { ChevronDown, Menu, ShoppingCart } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
 
 export function NavbarDrawer({ navItems }: { navItems: NavItemType[] }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const closeSheet = () => setIsOpen(false);
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger asChild className="lg:hidden">
-        <Button size="icon"  className="border h-8 w-8 rounded-md">
+        <Button size="icon" className="border h-8 w-8 rounded-md">
           <Menu color="white" className="size-6" />
         </Button>
       </SheetTrigger>
@@ -28,7 +35,6 @@ export function NavbarDrawer({ navItems }: { navItems: NavItemType[] }) {
         <SheetHeader>
           <SheetTitle>Menu</SheetTitle>
         </SheetHeader>
-
 
         <div className="flex flex-col  justify-start gap-4">
           {navItems.map((item) =>
@@ -45,8 +51,14 @@ export function NavbarDrawer({ navItems }: { navItems: NavItemType[] }) {
                 <CollapsibleContent className="pl-6 my-4 flex flex-col items-start gap-4">
                   {item.links.map((subItem) => (
                     <SheetClose key={subItem.label}>
-                      <Link href={subItem.href!} className="w-full text-left flex items-center">
-                        {subItem.icon && <span className="mr-2">{subItem.icon}</span>}
+                      <Link
+                        href={subItem.href!}
+                        className="w-full text-left flex items-center"
+                        onClick={() => setIsOpen(false)}
+                      >
+                        {subItem.icon && (
+                          <span className="mr-2">{subItem.icon}</span>
+                        )}
                         {subItem.label}
                       </Link>
                     </SheetClose>
@@ -54,27 +66,26 @@ export function NavbarDrawer({ navItems }: { navItems: NavItemType[] }) {
                 </CollapsibleContent>
               </Collapsible>
             ) : (
-              <SheetClose key={item.label}>
-                <Link href={item.href!} className="w-full text-left flex items-center">
-                  {item.icon && <span className="mr-2">{item.icon}</span>}
-                  {item.label}
-                </Link>
-              </SheetClose>
+              <Link
+                href={item.href!}
+                className="w-full text-left flex items-center"
+                key={item.href + item.label}
+                onClick={closeSheet}
+              >
+                {item.icon && <span className="mr-2">{item.icon}</span>}
+                {item.label}
+              </Link>
             )
           )}
 
           <div className="flex items-center justify-around mt-4">
-            <ShoppingCart className="h-6 w-6 text-gray-600 dark:text-gray-300 cursor-pointer" />
-
+            <Link href="/cart" onClick={closeSheet}>
+              <ShoppingCart className="h-6 w-6 text-gray-600 dark:text-gray-300 cursor-pointer" />
+            </Link>
             <ModeToggle />
           </div>
         </div>
-
-        {/* <SheetFooter>
-          
-        </SheetFooter> */}
-
       </SheetContent>
     </Sheet>
-  )
+  );
 }
